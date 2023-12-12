d09093e09db2fc24484949eb32dea5c3da8ada17
B-joon <dosel45@gmail.com>
2023-12-12 16:28:26 +0900

    - 제너릭 타입 수정 - hash 사용 방식 추가

--- src/main/java/com/example/springboot_redis/config/RedisConfig.java
+++ src/main/java/com/example/springboot_redis/config/RedisConfig.java
@@ -31,11 +31,17 @@ public class RedisConfig {
 
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<Object, Object> redisTemplate() {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
 
+        // 일반적인 key:value 방식의 Serializer
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        // hash 사용 방식의 Serializer
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
         redisTemplate.setConnectionFactory(redisConnectionFactory());
 
         return redisTemplate;
